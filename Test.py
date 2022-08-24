965350f1f3b3e0a5725639cfb261c552b6463dff
JosephKemper <JosephKemper@gmail.com>
2022-08-24 09:55:43 -0600

    Programming Hero Swap Variables challenge

--- Test.py
+++ Test.py
@@ -1,3 +1,10 @@
-x = int(input("Enter the First #: "))
-y = int(input("Enter the Second #: "))
-print(x//y)
+from tkinter import Y
+
+
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+swap = x
+x = y
+y = swap
+print(f"x={x}")
+print(f"y={y}")
