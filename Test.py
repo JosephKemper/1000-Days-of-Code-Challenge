06304be18f840b8971a58fba96b06c0a11a59fae
JosephKemper <JosephKemper@gmail.com>
2022-08-25 20:33:24 -0600

    Max of two coding challenge

--- Test.py
+++ Test.py
@@ -3,8 +3,10 @@ from tkinter import Y
 
 x = int(input("Enter x: "))
 y = int(input("Enter y: "))
-swap = x
-x = y
-y = swap
-print(f"x={x}")
-print(f"y={y}")
+
+if x > y:
+    print(x)
+elif y < x:
+    print(y)
+else:
+    print("Same")
