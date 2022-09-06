65e0974730f3abae3efa09fe99e724e759c70c11
JosephKemper <JosephKemper@gmail.com>
2022-09-06 10:46:16 -0600

    Working on test functions for sentences.py

--- Test.py
+++ Test.py
@@ -1,2 +1,46 @@
-# I think I have everything I want to know figured out. 
-# If I am right, this should be my final test.
+from sentences import get_determiner, get_noun, get_verb
+import random
+import pytest
+
+
+
+
+
+def test_past_tense():
+
+    for _ in range(4):
+        verb= get_verb(1, "past")
+        # Past Tense verbs
+        past_tense_verbs = ["drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"]
+        assert verb in past_tense_verbs
+
+def test_present_tense_singular ():
+    for _ in range(4):
+        verb= get_verb(1, "present")
+        # Present Tense singular verbs
+        singular_present_tense_verbs = ["drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"]
+        assert verb in singular_present_tense_verbs
+
+def test_present_tense_plural ():
+    for _ in range(4):
+        verb= get_verb(2, "plural")
+        # Present Tense plural verbs
+        plural_present_tense_verbs = ["drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"]
+        assert verb in plural_present_tense_verbs
+
+    for _ in range(4):
+        verb= get_verb(1, "future")
+        # Future Tense verbs
+        future_tense_verbs = ["will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"]
+        assert verb in future_tense_verbs
+
+
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
--- sentences.py
+++ sentences.py
@@ -1,4 +1,3 @@
-
 import random
 
 def main ():
