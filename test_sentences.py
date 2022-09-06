f78b3d500161278df8374a45440e970293bd8075
JosephKemper <JosephKemper@gmail.com>
2022-09-06 11:16:09 -0600

    Adjusted code to be more unified in form.

--- test_sentences.py
+++ test_sentences.py
@@ -56,33 +56,36 @@ def test_get_noun ():
 
 def test_get_verb():
 
+    # Past Tense verbs
+    past_tense_verbs = ["drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"]
     for _ in range(4):
         verb= get_verb(1, "past")
-        # Past Tense verbs
-        past_tense_verbs = ["drank", "ate", "grew", "laughed", "thought",
-        "ran", "slept", "talked", "walked", "wrote"]
-        assert verb in past_tense_verbs
 
+        assert verb in past_tense_verbs
+    
+    # Present Tense singular verbs
+    singular_present_tense_verbs = ["drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"]
     for _ in range(4):
         verb= get_verb(1, "present")
-        # Present Tense singular verbs
-        singular_present_tense_verbs = ["drinks", "eats", "grows", "laughs", "thinks",
-        "runs", "sleeps", "talks", "walks", "writes"]
         assert verb in singular_present_tense_verbs
+
+    # Present Tense plural verbs
+    plural_present_tense_verbs = ["drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"]
 
     for _ in range(4):
         verb= get_verb(2, "present")
-        # Present Tense plural verbs
-        plural_present_tense_verbs = ["drink", "eat", "grow", "laugh", "think",
-        "run", "sleep", "talk", "walk", "write"]
         assert verb in plural_present_tense_verbs
+
+    # Future Tense verbs
+    future_tense_verbs = ["will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"]
 
     for _ in range(4):
         verb= get_verb(1, "future")
-        # Future Tense verbs
-        future_tense_verbs = ["will drink", "will eat", "will grow", "will laugh",
-        "will think", "will run", "will sleep", "will talk",
-        "will walk", "will write"]
         assert verb in future_tense_verbs
 
 
