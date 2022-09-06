0de6a8f0e00461a53cfed06b281546adb5d979c0
JosephKemper <JosephKemper@gmail.com>
2022-09-05 18:13:49 -0600

    Finished HackerRank Lists challenge

--- HackerRank Lists.py
+++ HackerRank Lists.py
@@ -1,3 +1,13 @@
+"""
+Completed Hacker Rank Lists challenge. 
+https://www.hackerrank.com/challenges/python-lists/problem
+Thank you to https://www.codingbroz.com/lists-in-python-solution/ 
+for helping me figure out what the question was even asking. 
+While I did use their wonderfully built solution as a template for my own solution
+I did try to make it my own. 
+For example, I wanted to allow the commands to be entered in case insensitvely. 
+I also wanted to get more practice with functions. 
+"""
 def main ():
     number = int(input())
     dynamic_list(number)
@@ -25,9 +35,6 @@ def dynamic_list (number):
         else:
             pass
     return user_list
-
-
-
 
 
 if __name__ == '__main__':
