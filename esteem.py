53a61403f3b4469af3b76f9fa944b450641e0295
JosephKemper <JosephKemper@gmail.com>
2022-09-07 11:39:36 -0600

    Initial outline of 06 Team Activity completed

---
+++ esteem.py
@@ -0,0 +1,49 @@
+# 06 Team Activity: Troubleshooting Functions
+"""
+Problem Statement
+The Rosenberg self-esteem scale is a self-esteem measure developed by the sociologist Morris Rosenberg in 1965. 
+It is still used in social-science research today. 
+To complete the measure, a person completes a survey that contains the following ten statements.
+
+I feel that I am a person of worth, at least on an equal plane with others.
+I feel that I have a number of good qualities.
+All in all, I am inclined to feel that I am a failure.
+I am able to do things as well as most other people.
+I feel I do not have much to be proud of.
+I take a positive attitude toward myself.
+On the whole, I am satisfied with myself.
+I wish I could have more respect for myself.
+I certainly feel useless at times.
+At times I think I am no good at all.
+The person responds to each statement by choosing one of these four options: 
+strongly disagree, disagree, agree, or strongly agree. 
+The person's response to each statement is worth 0–3 points, 
+meaning the highest possible score is 10 * 3 = 30 points. 
+If a person scores lower than 15 points, the person may have a problematic low self-esteem.
+
+Notice that five of the statements (numbers 1, 2, 4, 6, 7) are positive and are scored like this:
+
+Choice	Points
+Strongly Disagree	0
+Disagree	1
+Agree	2
+Strongly Agree	3
+The other five statements (numbers 3, 5, 8, 9, 10) are negative and are scored like this:
+
+Choice	Points
+Strongly Disagree	3
+Disagree	2
+Agree	1
+Strongly Agree	0
+
+Core Requirements
+Your program prints the introductory text as shown in the Testing Procedure section below.
+Your program prints each of the ten statements and gets a response from the user.
+Your program computes the score for each response and sums all the scores and displays the total score.
+"""
+
+# TODO "Program prints the introductory text."
+
+# TODO "Program prints each of the ten statements and gets a response from the user."
+
+# TODO "Program computes the score for each response and sums all the scores and displays the total score."
