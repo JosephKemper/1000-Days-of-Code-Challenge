107b5fe26c9e3516625ba390226a309c457d3140
JosephKemper <JosephKemper@gmail.com>
2022-09-16 19:49:39 -0600

    Finished day 35 of 100 days of code

--- test_sentences.py
+++ test_sentences.py
@@ -88,7 +88,31 @@ def test_get_verb():
         verb= get_verb(1, "future")
         assert verb in future_tense_verbs
 
+# TODO: #9 #8 Write def test_get_preposition matching requirements outlined in 06 Prove Assignment
+"""Return a randomly chosen preposition
+    from this list of prepositions:
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
 
+    Return: a randomly chosen preposition.
+"""
+
+# TODO: #10 write def test_get_prepositional_phrase matching requirements outlined in 06 Prove Assignment
+"""
+    Build and return a prepositional phrase composed of three
+    words: a preposition, a determiner, and a noun by calling the
+    get_preposition, get_determiner, and get_noun functions.
+
+    Parameter
+        quantity: an integer that determines if the determiner
+            and noun in the prepositional phrase returned from
+            this function are single or pluaral.
+    Return: a prepositional phrase.
+"""
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
