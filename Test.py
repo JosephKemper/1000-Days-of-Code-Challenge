b44f588155a70f573aed81128fd4fdcb908ecd8a
JosephKemper <JosephKemper@gmail.com>
2022-10-17 20:08:33 -0600

    Finished Day 60

--- Test.py
+++ Test.py
@@ -1,5 +1,15 @@
-n = start
-    for elem in sequence:
-        yield n, elem
-        n += 1
-        
+import PySimpleGUI as sg      
+
+layout = [[sg.Text('demo')],      
+                 [sg.Text("name"),sg.InputText()],
+                 [sg.Text("age"),sg.InputText()],
+                 [sg.Text("tall"),sg.InputText()],
+                 [sg.Submit(), sg.Cancel()]]      
+
+window = sg.Window('Window Title', layout)    
+
+event, values = window.read()    
+window.close()
+
+text_input = values[0], values[1], values[2]
+sg.popup('Your info', text_input)
