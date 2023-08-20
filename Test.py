ed05aad3f0180ca57826fa2d6d3e5401c29af145
Joseph Kemper <JosephKemper@gmail.com>
2023-08-20 06:07:34 -0600

    Scheduled day off from coding

--- Test.py
+++ Test.py
@@ -1,20 +1,4 @@
-from queue import Queue
-
-my_queue = Queue()
-
-# Enqueue
-my_queue.put('a')
-my_queue.put('b')
-my_queue.put('c')
-
-# Dequeue
-item = my_queue.get()
-print(f"Dequeued item: {item}")
-
-# Find size
-size = my_queue.qsize()
-print(f"Approximate size of queue: {size}")
-
-# Check if empty
-is_empty = my_queue.empty()
-print(f"Is queue empty? {is_empty}")
+n=5
+while (n):
+    n=n-1
+    print(n,end=' ')
